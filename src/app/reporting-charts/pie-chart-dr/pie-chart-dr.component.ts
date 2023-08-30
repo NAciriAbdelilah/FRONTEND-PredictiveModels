6ad15912a8259510d1e7c5b4e33dea74b35d2142
Abdelilah Naciri <abdelilah.naciri@outlook.com>
2023-08-30 23:02:30 +0100

    update DR et MARCHE charts

--- src/app/reporting-charts/pie-chart-dr/pie-chart-dr.component.ts
+++ src/app/reporting-charts/pie-chart-dr/pie-chart-dr.component.ts
@@ -1,39 +1,33 @@
-import {Component, Input, OnChanges, OnInit, ViewChild} from '@angular/core';
-import {ChartConfiguration, ChartData, ChartType, Chart} from "chart.js";
-import {catchError, throwError} from "rxjs";
-import DatalabelsPlugin from "chartjs-plugin-datalabels";
-import {BaseChartDirective} from "ng2-charts";
+import { Component, Input, OnChanges, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType, Chart } from 'chart.js'; // Import Chart class
+import { catchError, throwError } from 'rxjs';
+import DatalabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-pie-chart-dr',
   templateUrl: './pie-chart-dr.component.html',
   styleUrls: ['./pie-chart-dr.component.css']
 })
-
 export class PieChartDrComponent implements OnInit, OnChanges {
-  @Input() reportingByDRData: any; // Define the input property
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  @Input() reportingByDRData: any;
+  @ViewChild('chartCanvas') chartCanvas: ElementRef | undefined; // Reference to the chart canvas
 
-  labels: string[] = []; // Initialize labels as an empty array
-  dataPoints: number[] = []; // Initialize dataPoints as an empty array
+  labels: string[] = [];
+  dataPoints: number[] = [];
   errorMessage: any;
-
 
   constructor() {}
 
   ngOnInit() {}
 
   ngOnChanges() {
-    console.log(this.reportingByDRData)
+    console.log(this.reportingByDRData);
     if (this.reportingByDRData) {
       this.createChart();
     }
   }
 
-  //-------------------------------------------------------------------------------------------------------
-
   createChart() {
-
     if (this.reportingByDRData) {
       this.reportingByDRData.pipe(
         catchError((err) => {
@@ -41,22 +35,64 @@ export class PieChartDrComponent implements OnInit, OnChanges {
           return throwError(() => new Error(err.message));
         })
       ).subscribe((data: any) => {
-        console.log('Reporting reportingByDRData:', data);
+        console.log('ReportingByDRData:', data);
 
         // Update chart data
         this.labels = data.map((item: any) => item.directionRegionale);
         this.dataPoints = data.map((item: any) => item.numberOfOutput);
 
-        // Ensure chart is initialized before trying to update it
-        if (this.chart) {
-          // Update chart data
-          this.chart.labels = this.labels;
-          // Check if datasets[0] and data exist
-          if (this.chart.datasets && this.chart.datasets.length > 0 && this.chart.datasets[0].data) {
-            this.chart.datasets[0].data = this.dataPoints;
+        // Create a new chart instance
+        if (this.chartCanvas) {
+          const chartCanvas = this.chartCanvas.nativeElement as HTMLCanvasElement;
+          const ctx = chartCanvas.getContext('2d');
+
+          if (ctx) {
+            new Chart(ctx, {
+              type: 'bar', // Set the chart type to pie
+              data: {
+                labels: this.labels,
+                datasets: [
+                  {
+                    data: this.dataPoints,
+                    backgroundColor: [
+                      'rgb(231, 72, 70)',
+                      // Add more colors as needed
+                    ],
+                  },
+                ],
+              },
+              options: {
+                responsive: true,
+                plugins: {
+                  legend: {
+                    display: true,
+                    position: 'top',
+                  },
+                  datalabels: {
+                    formatter: (value: any, ctx: any) => {
+                      if (ctx.chart.data.labels) {
+                        return ctx.chart.data.labels[ctx.dataIndex];
+                      }
+                    },
+                    anchor: 'center',
+                    align: 'center',
+                  },
+                  title: {
+                    display: true,
+                    text: 'Reporting du Modéle par Direction Régionale',
+                    font: {
+                      size: 12,
+                    },
+                  },
+                },
+                maintainAspectRatio: false,
+                aspectRatio: 1,
+                animation: {
+                  duration: 200,
+                },
+              },
+            });
           }
-          // Render the chart after updating data
-          this.chart?.ngOnChanges({});
         }
 
         console.log("DR :::::::::", this.labels);
@@ -65,71 +101,4 @@ export class PieChartDrComponent implements OnInit, OnChanges {
     }
   }
 
-
-  //-------------------------------------------------------------------------------------------------------
-
-  // Pie
-  public pieChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-        position: 'top',
-      },
-      datalabels: {
-        formatter: (value: any, ctx: any) => {
-          if (ctx.chart.data.labels) {
-            return ctx.chart.data.labels[ctx.dataIndex];
-          }
-        },
-        anchor: 'center', // Horizontal alignment
-        align: 'center',  // Vertical alignment
-      },
-      title: {
-        display: true,
-        text: 'Reporting du Modéle par Direction Régionale', // Set your desired title here
-        font: {
-          size: 12, // Set the font size for the title
-        },
-      },
-    },
-    maintainAspectRatio: false, // Add this line to allow custom sizing
-    aspectRatio: 1, // Adjust the aspect ratio as needed (1 is a square chart)
-    animation: {
-      duration: 200, // Set the animation duration here
-    },
-  };
-
-
-  public pieChartData: ChartData<'pie', number[], string | string[]> = {
-    labels: this.labels,
-    datasets: [
-      {
-        data: this.dataPoints,
-        backgroundColor: [
-          'rgb(231,72,70)',
-        ],
-      },
-    ],
-  };
-
-  public pieChartType: ChartType = 'pie';
-  public pieChartPlugins = [DatalabelsPlugin];
-
-
-  changeLegendPosition(): void {
-    if (this.pieChartOptions?.plugins?.legend) {
-      this.pieChartOptions.plugins.legend.position =
-        this.pieChartOptions.plugins.legend.position === 'left'
-          ? 'top'
-          : 'left';
-    }
-
-    this.chart?.render();
-  }
-//-------------------------------------------------------------------------------------------------------
-
-
 }
-
-
--- src/app/reporting-charts/pie-chart-marche/pie-chart-marche.component.ts
+++ src/app/reporting-charts/pie-chart-marche/pie-chart-marche.component.ts
@@ -1,7 +1,7 @@
-import {Component, Input, OnChanges, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, OnChanges, OnInit, ViewChild} from '@angular/core';
 import {BaseChartDirective} from "ng2-charts";
 import {catchError, throwError} from "rxjs";
-import {ChartConfiguration, ChartData, ChartType} from "chart.js";
+import {Chart, ChartConfiguration, ChartData, ChartType} from "chart.js";
 import DatalabelsPlugin from "chartjs-plugin-datalabels";
 
 @Component({
@@ -12,7 +12,7 @@ import DatalabelsPlugin from "chartjs-plugin-datalabels";
 export class PieChartMarcheComponent implements OnInit, OnChanges {
 
   @Input() reportingByMarcheData: any; // Define the input property
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  @ViewChild('chartCanvas') chartCanvas: ElementRef | undefined; // Reference to the chart canvas
 
   labels: string[] = []; // Initialize labels as an empty array
   dataPoints: number[] = []; // Initialize dataPoints as an empty array
@@ -31,9 +31,7 @@ export class PieChartMarcheComponent implements OnInit, OnChanges {
   }
 
   //-------------------------------------------------------------------------------------------------------
-
   createChart() {
-
     if (this.reportingByMarcheData) {
       this.reportingByMarcheData.pipe(
         catchError((err) => {
@@ -41,22 +39,64 @@ export class PieChartMarcheComponent implements OnInit, OnChanges {
           return throwError(() => new Error(err.message));
         })
       ).subscribe((data: any) => {
-        console.log('Reporting reportingByMarcheData:', data);
+        console.log('ReportingByMarcheData:', data);
 
         // Update chart data
         this.labels = data.map((item: any) => item.libelleMarche);
         this.dataPoints = data.map((item: any) => item.numberOfOutput);
 
-        // Ensure chart is initialized before trying to update it
-        if (this.chart) {
-          // Update chart data
-          this.chart.labels = this.labels;
-          // Check if datasets[0] and data exist
-          if (this.chart.datasets && this.chart.datasets.length > 0 && this.chart.datasets[0].data) {
-            this.chart.datasets[0].data = this.dataPoints;
+        // Create a new chart instance
+        if (this.chartCanvas) {
+          const chartCanvas = this.chartCanvas.nativeElement as HTMLCanvasElement;
+          const ctx = chartCanvas.getContext('2d');
+
+          if (ctx) {
+            new Chart(ctx, {
+              type: 'pie', // Set the chart type to pie|bar....
+              data: {
+                labels: this.labels,
+                datasets: [
+                  {
+                    data: this.dataPoints,
+                    backgroundColor: [
+                      'rgba(246,80,78,0.99)',
+                      // Add more colors as needed
+                    ],
+                  },
+                ],
+              },
+              options: {
+                responsive: true,
+                plugins: {
+                  legend: {
+                    display: true,
+                    position: 'top',
+                  },
+                  datalabels: {
+                    formatter: (value: any, ctx: any) => {
+                      if (ctx.chart.data.labels) {
+                        return ctx.chart.data.labels[ctx.dataIndex];
+                      }
+                    },
+                    anchor: 'center',
+                    align: 'center',
+                  },
+                  title: {
+                    display: true,
+                    text: 'Reporting du Modéle par Marchés',
+                    font: {
+                      size: 12,
+                    },
+                  },
+                },
+                maintainAspectRatio: false,
+                aspectRatio: 1,
+                animation: {
+                  duration: 200,
+                },
+              },
+            });
           }
-          // Render the chart after updating data
-          this.chart?.ngOnChanges({});
         }
 
         console.log("MARCHE :::::::::", this.labels);
@@ -64,69 +104,5 @@ export class PieChartMarcheComponent implements OnInit, OnChanges {
       });
     }
   }
-
-
-  //-------------------------------------------------------------------------------------------------------
-
-  // Pie
-  public pieChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-        position: 'top',
-      },
-      datalabels: {
-        formatter: (value: any, ctx: any) => {
-          if (ctx.chart.data.labels) {
-            return ctx.chart.data.labels[ctx.dataIndex];
-          }
-        },
-        anchor: 'center', // Horizontal alignment
-        align: 'center',  // Vertical alignment
-      },
-      title: {
-        display: true,
-        text: 'Reporting du Modéle par Marchés', // Set your desired title here
-        font: {
-          size: 12, // Set the font size for the title
-        },
-      },
-    },
-    maintainAspectRatio: false, // Add this line to allow custom sizing
-    aspectRatio: 1, // Adjust the aspect ratio as needed (1 is a square chart)
-    animation: {
-      duration: 200, // Set the animation duration here
-    },
-  };
-
-
-  public pieChartData: ChartData<'pie', number[], string | string[]> = {
-    labels: this.labels,
-    datasets: [
-      {
-        data: this.dataPoints,
-        backgroundColor: [
-          'rgb(231,72,70)',
-        ],
-      },
-    ],
-  };
-
-  public pieChartType: ChartType = 'pie';
-  public pieChartPlugins = [DatalabelsPlugin];
-
-
-  changeLegendPosition(): void {
-    if (this.pieChartOptions?.plugins?.legend) {
-      this.pieChartOptions.plugins.legend.position =
-        this.pieChartOptions.plugins.legend.position === 'left'
-          ? 'top'
-          : 'left';
-    }
-
-    this.chart?.render();
-  }
-
 
 }
